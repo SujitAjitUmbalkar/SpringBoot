23522d31f37a5022edd20d16884ade0e4c1cd4e8
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-12-27 22:43:23 +0530

    try

---
+++ Spring-Boot-Projects/WebLayer/src/main/java/com/Cohort/WebLayers/WebLayer/Entity/EmployeeEntity.java
@@ -0,0 +1,17 @@
+package com.Cohort.WebLayers.WebLayer.Entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="Employee")                 // if this is not done , then name of the table will be saved as EmployeeEntity in database
+public class EmployeeEntity
+{
+    private String name;
+    private Long id;
+    private String email;
+    private Integer age;
+    private LocalDate dateOfJoining;
+    private Boolean isactive;
+}
