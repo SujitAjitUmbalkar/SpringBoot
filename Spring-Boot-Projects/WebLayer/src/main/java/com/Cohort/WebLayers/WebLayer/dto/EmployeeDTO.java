8037e8bac24cddd01e218f4e272f947d23733e5f
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-01-16 22:15:37 +0530

    Added Validation Annotations

--- Spring-Boot-Projects/WebLayer/src/main/java/com/Cohort/WebLayers/WebLayer/dto/EmployeeDTO.java
+++ Spring-Boot-Projects/WebLayer/src/main/java/com/Cohort/WebLayers/WebLayer/dto/EmployeeDTO.java
@@ -1,7 +1,7 @@
 package com.Cohort.WebLayers.WebLayer.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,13 +15,36 @@ import java.time.LocalDate;
 @Setter
 public class EmployeeDTO
 {
-    @NotNull
-   private String name;
-   private Long id;
-   private String email;
-   private Integer age;
-   private LocalDate dateOfJoining;
-   @JsonProperty("isactive")                // using this we can use diffenent variable names in postman and in backend
-   private Boolean isactiveEmployee;
+    private Long id;
 
+    @NotBlank(message = "Name of the employee cannot be blank")
+    @Size(min = 3, max = 10, message = "Number of characters in name should be in the range: [3, 10]")
+    private String name;
+
+    @NotBlank(message = "Email of the employee cannot be blank")
+    @Email(message = "Email should be a valid email")
+    private String email;
+
+    @NotNull(message = "Age of the employee cannot be blank")
+    @Max(value = 80, message = "Age of Employee cannot be greater than 80")
+    @Min(value = 18, message = "Age of Employee cannot be less than 18")
+    private Integer age;
+
+    @NotBlank(message = "Role of the employee cannot be blank")
+    @Pattern(regexp = "^(ADMIN|USER)$", message = "Role of Employee can either be USER or ADMIN")
+    private String role; //ADMIN, USER
+
+    @NotNull(message = "Salary of Employee should be not null")
+    @Positive(message = "Salary of Employee should be positive")
+    @Digits(integer = 6, fraction = 2, message = "The salary can be in the form XXXXX.YY")
+    @DecimalMax(value = "100000.99")
+    @DecimalMin(value = "100.50")
+    private Double salary;
+
+    @PastOrPresent(message = "DateOfJoining field in Employee cannot be in the future")
+    private LocalDate dateOfJoining;
+
+    @AssertTrue(message = "Employee should be active")
+    @JsonProperty("isActive")
+    private Boolean isActive;
 }
