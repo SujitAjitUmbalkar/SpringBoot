e7eefd2a1afde802f21b009dcd56955d00ed6803
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-02-09 12:42:55 +0530

    Hibernate ORM Started ..

---
+++ Spring-Boot-Projects/Heibernate ORM , JPA/jpaTuts/src/main/java/com/codingshuttle/jpaTutorial/jpaTuts/entities/Product.java
@@ -0,0 +1,41 @@
+package com.codingshuttle.jpaTutorial.jpaTuts.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+
+public class Product
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String sku;
+
+    @Column(name = "title_x")
+    private String title;
+
+    private BigDecimal price;
+
+    private Integer quantity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
