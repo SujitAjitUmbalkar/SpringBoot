a245ff43f9a80b8b41d0056c24135a8ac6dafd16
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-02-26 21:34:13 +0530

    Added properties for devtools

--- Spring-Boot-Projects/Production ready features/prod-ready-features/src/main/java/com/codingshuttle/anuj/prod_ready_features/prod_ready_features/dto/PostDTO.java
+++ Spring-Boot-Projects/Production ready features/prod-ready-features/src/main/java/com/codingshuttle/anuj/prod_ready_features/prod_ready_features/dto/PostDTO.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class PostDTO
 {
-    private Long id;
+    private Long postid;
     private String title;
     private String description;
 }
