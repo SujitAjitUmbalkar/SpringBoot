e27410a5ed433a0151f2104fc1caf04b815d8666
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-03-14 13:08:20 +0530

    When threre is no implementation of UserDetailservice , 1. When there is **no implementation of UserDetailsService**, Spring Boot automatically configures default authentication, 2. Spring Security creates an InMemoryUserDetailsManager, 3. a default user (username = user) with a randomly generated password is created, 4. the generated password is printed in the console at application startup, 5. authentication uses this **in-memory user instead of loading users from a database**.

--- Spring-Boot-Projects/Spring Security/SecurityApplication/src/main/java/com/codingshuttle/SecurityApp/SecurityApplication/services/UserService.java
+++ Spring-Boot-Projects/Spring Security/SecurityApplication/src/main/java/com/codingshuttle/SecurityApp/SecurityApplication/services/UserService.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-@Service
+//@Service
 @RequiredArgsConstructor
 public class UserService implements UserDetailsService
 {
@@ -22,15 +22,11 @@ public class UserService implements UserDetailsService
 }
 
 /*
-
-**Why Default Password Is Not Printed (Spring Security)**
-
-1. When Spring Security is added, Spring Boot automatically creates a **default user** (`username = user`) and prints a **random password** in the console.
-2. This happens only when **no authentication-related beans** are defined in the project.
-3. If a custom `UserDetailsService` bean is created (like `UserService implements UserDetailsService`), Spring Boot assumes the developer will **handle authentication manually**.
-4. Because of this, Spring Boot **disables the default user and password generation**.
-5. Therefore **no password is printed in the console** when the application starts.
-6. Authentication will then use **database users loaded through `UserDetailsService` instead of the default in-memory user**.
-
-
+1. When there is **no implementation of `UserDetailsService`**,
+ Spring Boot automatically configures default authentication,
+ 2. Spring Security creates an `InMemoryUserDetailsManager`,
+  3. a default user (`username = user`) with a randomly generated password is created,
+  4. the generated password is printed in the console at application startup,
+   5. authentication uses this **in-memory user instead of loading users from a database**.
+6. if you have wrote properties , then passoword will be used but not printed on console
  */
