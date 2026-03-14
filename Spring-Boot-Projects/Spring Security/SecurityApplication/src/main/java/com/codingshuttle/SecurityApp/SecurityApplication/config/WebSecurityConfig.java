c63ba4fc63185e02affac49b36da25dac0e68d19
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-03-14 15:06:58 +0530

    Disabled SessionId - STATELESS

--- Spring-Boot-Projects/Spring Security/SecurityApplication/src/main/java/com/codingshuttle/SecurityApp/SecurityApplication/config/WebSecurityConfig.java
+++ Spring-Boot-Projects/Spring Security/SecurityApplication/src/main/java/com/codingshuttle/SecurityApp/SecurityApplication/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -26,6 +27,8 @@ public class WebSecurityConfig
                          .requestMatchers("/posts/**").hasAnyRole("ADMIN")
                          .anyRequest().authenticated())
                  .csrf(cssrf -> cssrf.disable())
+                 .sessionManagement(sessionConfig->sessionConfig
+                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                  .formLogin(Customizer.withDefaults());
          return  httpSecurity.build();
      }
