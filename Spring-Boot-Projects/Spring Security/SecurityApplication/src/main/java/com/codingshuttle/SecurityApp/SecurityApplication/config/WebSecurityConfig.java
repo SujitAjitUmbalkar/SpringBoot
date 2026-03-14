e5630daa205d8269afc55f3fdbb0c19e0d4a1973
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-03-14 13:03:00 +0530

    Implemented UserDetailsService ,  1. When we implement UserDetailsService, Spring Security stops using default in-memory user configuration, 2. properties spring.security.user.name and spring.security.user.password are ignored, 3. authentication loads users through loadUserByUsername() method, 4. user details are fetched from database using repository, 5. password is verified using PasswordEncoder

--- Spring-Boot-Projects/Spring Security/SecurityApplication/src/main/java/com/codingshuttle/SecurityApp/SecurityApplication/config/WebSecurityConfig.java
+++ Spring-Boot-Projects/Spring Security/SecurityApplication/src/main/java/com/codingshuttle/SecurityApp/SecurityApplication/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.codingshuttle.SecurityApp.SecurityApplication.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,13 +11,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurityConfig
 {
+    @Bean
      SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception
      {
          httpSecurity
                  .authorizeHttpRequests(auth->auth
                          .anyRequest().authenticated())
                  .formLogin(Customizer.withDefaults());
-
          return  httpSecurity.build();
      }
 }
--- Spring-Boot-Projects/Spring Security/SecurityApplication/src/main/java/com/codingshuttle/SecurityApp/SecurityApplication/services/UserService.java
+++ Spring-Boot-Projects/Spring Security/SecurityApplication/src/main/java/com/codingshuttle/SecurityApp/SecurityApplication/services/UserService.java
@@ -7,8 +7,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class UserService implements UserDetailsService
 {
     private final UserRepository userRepository;
@@ -17,6 +17,20 @@ public class UserService implements UserDetailsService
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
     {
         return userRepository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException(username));
+                .orElseThrow(() -> new UsernameNotFoundException( "user with "+ username+" not found."));
     }
 }
+
+/*
+
+**Why Default Password Is Not Printed (Spring Security)**
+
+1. When Spring Security is added, Spring Boot automatically creates a **default user** (`username = user`) and prints a **random password** in the console.
+2. This happens only when **no authentication-related beans** are defined in the project.
+3. If a custom `UserDetailsService` bean is created (like `UserService implements UserDetailsService`), Spring Boot assumes the developer will **handle authentication manually**.
+4. Because of this, Spring Boot **disables the default user and password generation**.
+5. Therefore **no password is printed in the console** when the application starts.
+6. Authentication will then use **database users loaded through `UserDetailsService` instead of the default in-memory user**.
+
+
+ */
