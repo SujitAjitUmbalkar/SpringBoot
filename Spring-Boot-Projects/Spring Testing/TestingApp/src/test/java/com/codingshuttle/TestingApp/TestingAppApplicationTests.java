80744020434afe16933b19af6c70c15ee07e2d67
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-03-15 00:03:24 +0530

    use @Disabled to disable any method (use when you want to run all methods except one )

--- Spring-Boot-Projects/Spring Testing/TestingApp/src/test/java/com/codingshuttle/TestingApp/TestingAppApplicationTests.java
+++ Spring-Boot-Projects/Spring Testing/TestingApp/src/test/java/com/codingshuttle/TestingApp/TestingAppApplicationTests.java
@@ -1,14 +1,16 @@
 package com.codingshuttle.TestingApp;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class TestingAppApplicationTests
 {
 
 	@Test
+    @Disabled
 	void contextLoads()
     {
 
