9a3d08bff785bfa9d1fbdea114aeeeb5af2deb9e
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-03-15 20:03:27 +0530

    Added JUnit Annotations , used Lombok , @Slf4J , used lifecycle methods (NOTES - Added notes for JUnit lifecycle annotations used in tests: 1) @BeforeEach – runs before every test (BeforeEach → test1 → BeforeEach → test2 ...), 2) @AfterEach – runs after every test (AfterEach → test1 → AfterEach → test2 ...), 3) @BeforeAll – runs once before all tests (BeforeAll → test1 → test2 → test3 ...; method must be static), 4) @AfterAll – runs once after all tests (test1 → test2 → test3 → AfterAll).

--- Spring-Boot-Projects/Spring Testing/TestingApp/src/test/java/com/codingshuttle/TestingApp/TestingAppApplicationTests.java
+++ Spring-Boot-Projects/Spring Testing/TestingApp/src/test/java/com/codingshuttle/TestingApp/TestingAppApplicationTests.java
@@ -1,26 +1,41 @@
 package com.codingshuttle.TestingApp;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 //@SpringBootTest
+@Slf4j
 class TestingAppApplicationTests
 {
 
 	@Test
     @Disabled
 	void contextLoads()
-    {
+    { }
 
-	}
+    @BeforeEach
+    void setUp(){log.info("before each test , used to setup something before each test  ");}
+
+    @AfterEach
+    void setDown(){log.info("After each test , used to setdown  resources  before each test  ");}
+
+    @BeforeAll
+  static   void setUpOnce(){log.info("before all tests , used to setup something before all tests ");}
+
+    @AfterAll
+    static void setDownOnce() {log.info("after all tests , used to setDown something after all tests");}
 
     @Test
-    @DisplayName("DisplayTestNameAsanyTest")
-    void anyTest()
-    {
+    void testOne() { log.info("test one is running"); }
 
-    }
+    @Test
+    void testTwo() { log.info("test two is running"); }
 
 }
+
+
+// @BeforeEach runs before every test ( beforeeach , 1test , beforeeach 2nd test ... )
+// @AfterEach runs after every test ( AfterEach , 1test , AfterEach 2nd test ... )
+// @BeforeAll runs before all tests ( BeforeAll , test 1 , test2 , test3... ) (method should be static
+// @AfterAll runs after all tests (test1 , test2 , test3 ... AfterAll )
