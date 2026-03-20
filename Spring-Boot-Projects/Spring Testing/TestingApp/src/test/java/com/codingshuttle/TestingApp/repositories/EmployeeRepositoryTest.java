35fb1f315896d4226bacc97c4098633c5e7b387c
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-03-20 20:48:37 +0530

    Replacing Database for test (H2 used ) , dependancy added

--- Spring-Boot-Projects/Spring Testing/TestingApp/src/test/java/com/codingshuttle/TestingApp/repositories/EmployeeRepositoryTest.java
+++ Spring-Boot-Projects/Spring Testing/TestingApp/src/test/java/com/codingshuttle/TestingApp/repositories/EmployeeRepositoryTest.java
@@ -1,11 +1,15 @@
 package com.codingshuttle.TestingApp.repositories;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class EmployeeRepositoryTest {
-
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)     // another temp database will be used for tests
+class EmployeeRepositoryTest
+{
     @Test
     void testFindByEmail_whenEmailIsPresent_thenReturnEmployee()            // renamed
     {
