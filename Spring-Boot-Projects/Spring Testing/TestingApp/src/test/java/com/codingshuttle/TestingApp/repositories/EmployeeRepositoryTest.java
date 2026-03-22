33231f3f247f75d0122633ed1772aa255bd8d534
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-03-22 21:37:29 +0530

    implemented Arrange-Act-Assert pattern where @BeforeEach is used to create test resources (Employee object), test methods handle Arrange by saving data using employeeRepository.save(), Act by calling repository method findByEmail(), and Assert by validating results using AssertJ assertions like isNotEmpty(), isEmpty(), and isEqualTo(), ensured correct dependency injection using @Autowired to avoid NullPointerException, followed best practices by keeping @BeforeEach only for object creation (no DB interaction), using isNotEmpty() instead of redundant isNotNull(), verifying actual data correctness instead of just existence, maintaining test independence as each test runs in transaction and rolls back automatically

--- Spring-Boot-Projects/Spring Testing/TestingApp/src/test/java/com/codingshuttle/TestingApp/repositories/EmployeeRepositoryTest.java
+++ Spring-Boot-Projects/Spring Testing/TestingApp/src/test/java/com/codingshuttle/TestingApp/repositories/EmployeeRepositoryTest.java
@@ -1,10 +1,16 @@
 package com.codingshuttle.TestingApp.repositories;
 
+import com.codingshuttle.TestingApp.entities.Employee;
 import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 // @SpringBootTest
 @DataJpaTest
@@ -14,15 +20,61 @@ class EmployeeRepositoryTest
     @Autowired
     private  EmployeeRepository employeeRepository;
 
+//        1. create resources ,
+//        2. Arrange , given
+//        3.  Act , when
+//        4. Assert , then
+
+    private Employee employee;      // 1.  create resources
+
+    @BeforeEach                 //   run before each method
+    void setUp()
+    {
+        //        1. provide resources
+        employee = Employee.builder()                       // 1. created resource
+                .id(5L)
+                .email("jeetumbalkar@gmail.com")
+                .salary(50000L)
+                .name("Sujit")
+                .build();
+    }
+
+    // @BeforeEach → only create objects
+    //@Test → save + execute + assert
+
     @Test
     void testFindByEmail_whenEmailIsPresent_thenReturnEmployee()            // renamed
     {
-        employeeRepository.findByEmail(" ");
+//        2. Arrange , given
+        employeeRepository.save(employee);
+
+//        3.  Act , when
+        List<Employee> employeeList  = employeeRepository.findByEmail(employee.getEmail());
+
+    //        4. Assert , then
+        assertThat(employeeList).isNotEmpty();
+        assertThat(employeeList).isNotNull();
+
+//        isNotNull() → only checks object exists
+//        isNotEmpty() → checks object exists + has data
+
+        // check we fetched right emplyee
+        assertThat(employeeList.get(0).getEmail()).isEqualTo(employee.getEmail());
+
+
     }
 
     @Test
     void testFindByEmail_whenEmailIsNotFound_thenReturnEmptyEmployeeList()
     {
+        String email = "notFound@gmail.com";
+
+        List<Employee> employeeList  = employeeRepository.findByEmail(email);
+
+        assertThat(employeeList).isEmpty();
+        assertThat(employeeList).isNotNull();
+
+
 
     }
 }
