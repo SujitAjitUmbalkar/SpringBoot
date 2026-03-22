3410e6484974bfa31a7077afd65ab32a092c3837
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-03-22 18:53:07 +0530

    Use @DataJpaTest to perform fast and focused repository layer testing by loading only JPA components like entities and repositories with an in-memory database, avoiding full application startup overhead and ensuring isolated, transactional tests with automatic rollback

--- Spring-Boot-Projects/Spring Testing/TestingApp/src/test/java/com/codingshuttle/TestingApp/repositories/EmployeeRepositoryTest.java
+++ Spring-Boot-Projects/Spring Testing/TestingApp/src/test/java/com/codingshuttle/TestingApp/repositories/EmployeeRepositoryTest.java
@@ -1,19 +1,23 @@
 package com.codingshuttle.TestingApp.repositories;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
+// @SpringBootTest
+@DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)     // another temp database will be used for tests
 class EmployeeRepositoryTest
 {
+    @Autowired
+    private  EmployeeRepository employeeRepository;
+
     @Test
     void testFindByEmail_whenEmailIsPresent_thenReturnEmployee()            // renamed
     {
-
+        employeeRepository.findByEmail(" ");
     }
 
     @Test
