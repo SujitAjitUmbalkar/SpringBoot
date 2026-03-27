e20b617f2b339f12c068f84a51a6812430ce51a3
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-03-27 22:18:57 +0530

    Used @Spy instead of @Mock for modelmapper , @Spy is used in service layer testing when you want a real object to execute its actual logic, but still keep the option to mock specific methods if needed. In many service tests (like yours), dependencies such as ModelMapper contain important internal logic (e.g., object-to-DTO mapping). If you use @Mock, that logic won’t run and will return null, breaking your test. By using @Spy, you allow the real behavior to execute—ensuring correct mapping—while still having control to override parts if required. This helps you test the service’s business logic accurately without losing essential helper functionality.

--- Spring-Boot-Projects/Spring Testing/TestingApp/src/test/java/com/codingshuttle/TestingApp/services/Impl/EmployeeServiceImplTest.java
+++ Spring-Boot-Projects/Spring Testing/TestingApp/src/test/java/com/codingshuttle/TestingApp/services/Impl/EmployeeServiceImplTest.java
@@ -1,14 +1,23 @@
 package com.codingshuttle.TestingApp.services.Impl;
 
+import com.codingshuttle.TestingApp.dto.EmployeeDto;
+import com.codingshuttle.TestingApp.entities.Employee;
 import com.codingshuttle.TestingApp.repositories.EmployeeRepository;
-import com.codingshuttle.TestingApp.services.EmployeeService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.shaded.org.apache.commons.lang3.ObjectUtils;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
 
 @SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)     // another temp database will be used for tests
@@ -17,7 +26,8 @@ class EmployeeServiceImplTest
     @Mock
     EmployeeRepository employeeRepository;
 
-    @Mock
+//    @Mock
+    @Spy
     ModelMapper modelMapper;
 
     @InjectMocks            // mocks will be injected here
@@ -26,7 +36,24 @@ class EmployeeServiceImplTest
     @Test
     void testgetEmployeeById_whenEmployeeIdIsPresent_ThenReturnEmployeeDTO()
     {
-        employeeService.getEmployeeById(1L);
+        // arrange
+        Long id = 1L;
+        Employee mockedEmployee = Employee.builder()
+                .id(id)
+                .name("Sujit")
+                .email("jeetumbalkar@gmail.com")
+                .salary(50000L)
+                .build();
+
+        when(employeeRepository.findById(id)).thenReturn(Optional.of(mockedEmployee));              // stubbing
+
+        //act
+        EmployeeDto employeeDto = employeeService.getEmployeeById(id);
+
+        // Assert
+        assertThat(employeeDto.getId()).isEqualTo(id);
+        assertThat(  employeeDto.getEmail()   ).isEqualTo(mockedEmployee.getEmail());
+
     }
 
 }
