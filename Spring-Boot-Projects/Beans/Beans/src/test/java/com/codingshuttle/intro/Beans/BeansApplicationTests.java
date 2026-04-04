96ae67899236773171de54b768f05ad0ac93a1b5
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-04-04 11:05:26 +0530

    Starting Beans Concepts practically

---
+++ Spring-Boot-Projects/Beans/Beans/src/test/java/com/codingshuttle/intro/Beans/BeansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.intro.Beans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
