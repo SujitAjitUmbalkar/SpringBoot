589e34f0763c3967d460ac22e15a30381fc847fd
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-04-05 21:50:42 +0530

    Scope('session') is used

--- Spring-Boot-Projects/Beans/Beans/src/main/java/com/codingshuttle/intro/Beans/AppConfig.java
+++ Spring-Boot-Projects/Beans/Beans/src/main/java/com/codingshuttle/intro/Beans/AppConfig.java
@@ -7,20 +7,10 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class AppConfig
 {
-//    @Bean
-//    @Scope("prototype")   // 🔹 Prototype Scope
-//    // A NEW object is created every time it is requested from container
-//    // Example: getBean() called 5 times → 5 different objects
-//    public PaymentService paymentService()
-//    {
-//        // More logic
-//        return new PaymentService();
-//    }
-
     @Bean
-    @Scope("request")     // 🔹 Request Scope (WEB ONLY)
-    // One object per HTTP request
-    // New bean created for each incoming request
+    @Scope("session")     // 🔹 Session Scope (WEB ONLY)
+    // One object per HTTP session
+    // Same bean used across multiple requests in same session
     public PaymentService paymentService()
     {
         // More logic
