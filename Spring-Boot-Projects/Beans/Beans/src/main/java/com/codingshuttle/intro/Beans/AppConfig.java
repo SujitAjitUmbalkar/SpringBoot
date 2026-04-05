ce68e77370b18562205e4555f1926d8d06b376fb
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-04-05 21:49:17 +0530

    Scope('request') is used

--- Spring-Boot-Projects/Beans/Beans/src/main/java/com/codingshuttle/intro/Beans/AppConfig.java
+++ Spring-Boot-Projects/Beans/Beans/src/main/java/com/codingshuttle/intro/Beans/AppConfig.java
@@ -7,14 +7,25 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class AppConfig
 {
+//    @Bean
+//    @Scope("prototype")   // 🔹 Prototype Scope
+//    // A NEW object is created every time it is requested from container
+//    // Example: getBean() called 5 times → 5 different objects
+//    public PaymentService paymentService()
+//    {
+//        // More logic
+//        return new PaymentService();
+//    }
+
     @Bean
-    @Scope("prototype")   // 🔹 Prototype Scope
-    // A NEW object is created every time it is requested from container
-    // Example: getBean() called 5 times → 5 different objects
+    @Scope("request")     // 🔹 Request Scope (WEB ONLY)
+    // One object per HTTP request
+    // New bean created for each incoming request
     public PaymentService paymentService()
     {
         // More logic
         return new PaymentService();
     }
+
 
 }
