f35f362e656b77a1269375e73689c6a555ab853f
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-04-05 20:46:27 +0530

    Added run method from commandline interface , this method runs after bean has been injected successfully  , flow -> app starts main() → SpringApplication.run() → Spring Context Initialization → Bean Creation & Autowiring → CommandLineRunner.run() execution

--- Spring-Boot-Projects/Beans/Beans/src/main/java/com/codingshuttle/intro/Beans/BeansApplication.java
+++ Spring-Boot-Projects/Beans/Beans/src/main/java/com/codingshuttle/intro/Beans/BeansApplication.java
@@ -1,13 +1,27 @@
 package com.codingshuttle.intro.Beans;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BeansApplication {
-
+public class BeansApplication implements CommandLineRunner
+{
 	public static void main(String[] args) {
 		SpringApplication.run(BeansApplication.class, args);
 	}
 
+    @Autowired
+    PaymentService paymentService;
+
+    @Override
+    public void run(String... args) throws Exception
+    {
+        paymentService.pay();
+    }
+
+//    Used for triggering initial operations like service calls (paymentService.pay())
+//Ensures execution happens only after all beans are created and dependency injection is complete
+//Application flow: main() → SpringApplication.run() → Spring Context Initialization → Bean Creation & Autowiring → CommandLineRunner.run() execution
 }
